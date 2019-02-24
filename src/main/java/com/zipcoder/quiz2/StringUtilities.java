2b9dcc3fbdb7af7440be12d69f78ad665e4424a3
Nhu Nguyen <nhu313@gmail.com>
2019-02-23 20:11:01 -0500

    added string

---
+++ src/main/java/com/zipcoder/quiz2/StringUtilities.java
@@ -0,0 +1,22 @@
+package com.zipcoder.quiz2;
+
+
+public class StringUtilities {
+
+
+    public static Boolean isNotNull(String str) {
+        return null;
+    }
+
+    public static String firstAndLast(String value) {
+        return null;
+    }
+
+    public static String change(String value, char oldChar, char newChar) {
+        return null;
+    }
+
+    public static String[] separateByExaclaimation(String value) {
+        return null;
+    }
+}
---
+++ src/test/java/StringUtilitiesTest.java
@@ -0,0 +1,114 @@
+import com.zipcoder.quiz2.StringUtilities;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class StringUtilitiesTest {
+
+    @Test
+    public void testIsNotNull_whenIsNull() {
+        //Given
+        Boolean expected = false;
+
+        //When
+        Boolean actual = StringUtilities.isNotNull(null);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testIsNotNull_whenIsEmptyString() {
+        //Given
+        Boolean expected = true;
+
+        //When
+        Boolean actual = StringUtilities.isNotNull("");
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testFirstAndLast(){
+        //Given
+        String value = "world";
+        String expected = "wd";
+
+        //When
+        String actual = StringUtilities.firstAndLast(value);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testFirstAndLast_withSpace(){
+        //Given
+        String value = "Hello, world";
+        String expected = "Hd";
+
+        //When
+        String actual = StringUtilities.firstAndLast(value);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testChange(){
+        //Given
+        String value = "world";
+        String expected = "wyrld";
+
+        //When
+        String actual = StringUtilities.change(value, 'o', 'y');
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testChange_atTheEnd(){
+        //Given
+        String value = "world";
+        String expected = "worly";
+
+        //When
+        String actual = StringUtilities.change(value, 'd', 'y');
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSeparateByExaclaimation_whenThereIsNoComma(){
+        //Given
+        String value = "Hello world";
+        String[] expected = new String[]{"Hello world"};
+
+        //When
+        String[] actual = StringUtilities.separateByExaclaimation(value);
+
+        //Then
+        Assert.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testSeparateByExaclaimation(){
+        //Given
+        String value = "this!is!a!string!test";
+        String[] expected = new String[]{"this", "is", "a", "string", "test"};
+
+        //When
+        String[] actual = StringUtilities.separateByExaclaimation(value);
+        System.out.println(Arrays.toString(actual));
+        //Then
+        Assert.assertArrayEquals(expected, actual);
+    }
+
+
+
+
+}
